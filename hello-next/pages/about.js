78c2b94074ceeb78f36c0dd027c68007c1eea75d
kimseulbi <ksb5166@gmail.com>
2019-07-29 23:31:05 +0900

    file delect

--- hello-next/pages/about.js
+++
@@ -1,8 +0,0 @@
-import Layout from "../components/MyLayout";
-
-export default () => (
-    <Layout>
-        <p> This is the about page</p>
-        <button onClick={() => alert('뒤로갑시다')}>go back</button>
-    </Layout>
-)
